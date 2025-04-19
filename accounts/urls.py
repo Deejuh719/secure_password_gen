1ec40a6dec762529f929416f135726ecc678488a
K Surratt <deejuh719@gmail.com>
2025-04-18 20:05:43 -0400

    updated user account settings, some fixes still needed. added minor interface changes/buttons to certain pages as well as cosmetic updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import SignUpView
+from .views import (SignUpView, PasswordChangeView, PasswordChangeDoneView,
+                    AccountDeleteView, AccountSettingsView)
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("password_change/", PasswordChangeView.as_view(), name="password_change"),
+    path("password_change/done/", PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("account_settings/", AccountSettingsView.as_view(), name="account_settings"),
+    path("account_delete/", AccountDeleteView.as_view(), name="account_confirm_delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, DeleteView
+from django.contrib.auth import logout
+from django.contrib.messages.views import SuccessMessageMixin
+from .models import CustomUser
 
 # Create your views here.
 
@@ -8,3 +14,32 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class PasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    success_url = reverse_lazy("password_change_done")
+    template_name = "registration/password_change_form.html"
+
+class PasswordChangeDoneView(LoginRequiredMixin, TemplateView):
+    template_name = "registration/password_change_done.html"
+
+class AccountDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    template_name = "accounts/account_confirm_delete.html"
+    model = CustomUser
+    success_url = reverse_lazy("home")
+
+    def get_object(self):
+        return self.request.user
+    
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return super().delete(request, *args, **kwargs)
+    
+class AccountSettingsView(LoginRequiredMixin, TemplateView):
+    model = CustomUser
+    success_url = reverse_lazy("account_settings")
+    template_name = "accounts/account_settings.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user"] = self.request.user
+        return context
